e1d8660c51047e5b54f9f789bd4183fc6b756bfd
YonHoshihara <hoshiharyon@gmail.com>
2018-11-12 02:27:51 -0400

    Change screens

--- main.py
+++ main.py
@@ -1,60 +1,53 @@
 from Encrypt import  Chat_lib
 from tkinter import *
 
-class Application:
-    '''
-    class to manipulate the interface methods
-    for eacth eignet need a container
-    '''
+LARGE_FONT = ("Verdana", 12)
 
-    def insert_user(self):
-        user = self.txtusuario.get()
-        password = self.txtsenha.get()
-        Chat_lib.register(user, password)
 
-    def __init__(self, master=None):
+class SeaofBTCapp(Tk):
+
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+        container = Frame(self)
+
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (Login, PageOne, Register):
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(Login)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class Login(Frame):
+
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        label = Label(self, text="Login", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
         self.fonte = ("Verdana", "8")
-
-        self.container1 = Frame(master)
-        self.container1["pady"] = 10
-        self.container1.pack()
-        self.container2 = Frame(master)
-        self.container2["padx"] = 20
-        self.container2["pady"] = 5
-        self.container2.pack()
-        self.container3 = Frame(master)
-        self.container3["padx"] = 20
-        self.container3["pady"] = 5
-        self.container3.pack()
-        self.container4 = Frame(master)
-        self.container4["padx"] = 20
-        self.container4["pady"] = 5
-        self.container4.pack()
-        self.container5 = Frame(master)
-        self.container5["padx"] = 20
-        self.container5["pady"] = 5
-        self.container5.pack()
-        self.container6 = Frame(master)
+        self.container6 = Frame(self)
         self.container6["padx"] = 20
         self.container6["pady"] = 5
         self.container6.pack()
-        self.container7 = Frame(master)
+        self.container7 = Frame(self)
         self.container7["padx"] = 20
         self.container7["pady"] = 5
         self.container7.pack()
-        self.container8 = Frame(master)
-        self.container8["padx"] = 20
-        self.container8["pady"] = 10
-        self.container8.pack()
-        self.container9 = Frame(master)
-        self.container9["pady"] = 15
-        self.container9.pack()
 
-        self.titulo = Label(self.container1, text="Informe os dados :")
-        self.titulo["font"] = ("Calibri", "9", "bold")
-        self.titulo.pack()
-
-        self.lblusuario = Label(self.container6, text="Login:", font=self.fonte, width=10)
+        self.lblusuario = Label(self.container6, text="Usuário:", font=self.fonte, width=10)
         self.lblusuario.pack(side=LEFT)
 
         self.txtusuario = Entry(self.container6)
@@ -71,29 +64,74 @@ class Application:
         self.txtsenha["font"] = self.fonte
         self.txtsenha.pack(side=LEFT)
 
-        self.bntInsert = Button(self.container8, text="Inserir", font=self.fonte, width=12)
-        self.bntInsert["command"] = self.insert_user
-        self.bntInsert.pack(side=LEFT)
-        #
-        # self.bntAlterar = Button(self.container8, text="Alterar", font=self.fonte, width=12)
-        # self.bntAlterar["command"] = self.alterarUsuario
-        # self.bntAlterar.pack(side=LEFT)
-        #
-        # self.bntExcluir = Button(self.container8, text="Excluir", font=self.fonte, width=12)
-        # self.bntExcluir["command"] = self.excluirUsuario
-        # self.bntExcluir.pack(side=LEFT)
-        #
-        # self.lblmsg = Label(self.container9, text="")
-        # self.lblmsg["font"] = ("Verdana", "9", "italic")
-        # self.lblmsg.pack()
-
-        def insert_user(self):
-            user = self.txtusuario.get()
-            password = self.txtsenha.get()
-            Chat_lib.register(user, password)
 
 
+        button = Button(self, text="Login",
+                           command=lambda: controller.show_frame(PageOne))
+        button.pack()
 
-root = Tk()
-Application(root)
-root.mainloop()
+        button2 = Button(self, text="Register",
+                         command=lambda: controller.show_frame(Register))
+        button2.pack()
+
+
+class PageOne(Frame):
+
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        label = Label(self, text="Page One!!!", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = Button(self, text="Back to Home",
+                         command=lambda: controller.show_frame(Login))
+        button1.pack()
+
+        button2 = Button(self, text="Page Two",
+                         command=lambda: controller.show_frame(Register))
+        button2.pack()
+
+
+class Register(Frame):
+
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        label = Label(self, text="Register", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        self.fonte = ("Verdana", "8")
+        self.container6 = Frame(self)
+        self.container6["padx"] = 20
+        self.container6["pady"] = 5
+        self.container6.pack()
+        self.container7 = Frame(self)
+        self.container7["padx"] = 20
+        self.container7["pady"] = 5
+        self.container7.pack()
+
+        self.lblusuario = Label(self.container6, text="Usuário:", font=self.fonte, width=10)
+        self.lblusuario.pack(side=LEFT)
+
+        self.txtusuario = Entry(self.container6)
+        self.txtusuario["width"] = 25
+        self.txtusuario["font"] = self.fonte
+        self.txtusuario.pack(side=LEFT)
+
+        self.lblsenha = Label(self.container7, text="Senha:", font=self.fonte, width=10)
+        self.lblsenha.pack(side=LEFT)
+
+        self.txtsenha = Entry(self.container7)
+        self.txtsenha["width"] = 25
+        self.txtsenha["show"] = "*"
+        self.txtsenha["font"] = self.fonte
+        self.txtsenha.pack(side=LEFT)
+
+        button = Button(self, text="Login",
+                        command=lambda: controller.show_frame(Login))
+        button.pack()
+
+        button2 = Button(self, text="Register",
+                         command=lambda: controller.show_frame(Register))
+        button2.pack()
+
+
+app = SeaofBTCapp()
+app.mainloop()
